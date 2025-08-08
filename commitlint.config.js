e15ede04602b3b3e1a7b588730db587b7ef10952
lzjqsdd <lzjqsdd@gmail.com>
2025-08-08 22:40:40 +0800

    style: remove issue require

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,34 +1,5 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  plugins: [
-    {
-      rules: {
-        'require-issue-reference': (parsed) => {
-          const { subject } = parsed;
-          // Check if subject contains issue reference in format (#number)
-          const issuePattern = /\(#\d+\)$/;
-          
-          if (!issuePattern.test(subject)) {
-            return [
-              false,
-              'Commit message must contain issue reference in format (#number), e.g.: "feat: add new feature (#123)"'
-            ];
-          }
-          
-          // Check if subject (excluding issue reference) starts with lowercase
-          const subjectWithoutIssue = subject.replace(/\s*\(#\d+\)$/, '');
-          if (!/^[a-z]/.test(subjectWithoutIssue)) {
-            return [
-              false,
-              'Commit subject must start with lowercase letter'
-            ];
-          }
-          
-          return [true];
-        }
-      }
-    }
-  ],
   rules: {
     // Type must be one of the following
     'type-enum': [
@@ -54,15 +25,13 @@ module.exports = {
     'subject-empty': [2, 'never'],
     // Subject cannot end with period
     'subject-full-stop': [2, 'never', '.'],
-    // Disable default subject-case rule, use custom rule instead
-    'subject-case': [0],
-    // Header max length (increased to accommodate issue ID)
+    // Subject must start with lowercase
+    'subject-case': [2, 'always', 'lower-case'],
+    // Header max length
     'header-max-length': [2, 'always', 120],
     // Body line max length
     'body-max-line-length': [2, 'always', 100],
     // Footer line max length  
-    'footer-max-line-length': [2, 'always', 100],
-    // Use custom rule to validate issue reference
-    'require-issue-reference': [2, 'always']
+    'footer-max-line-length': [2, 'always', 100]
   }
 }; 
